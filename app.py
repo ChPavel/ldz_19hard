233548a0d3214ec79f3bbe3067e62c9cd79dfacf
Павел <chernovpp@gmail.com>
2022-08-02 21:59:42 +0700

    последние изменения

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from flask_restx import Api
-
 from config import Config
+from dao.model.user import User
 from setup_db import db
 from views.directors import director_ns
 from views.genres import genre_ns
 from views.movies import movie_ns
+from views.user import user_ns
 
 
 def create_app(config_object):
     app = Flask(__name__)
-    app.config.from_object(config_object)
+    app.url_map.strict_slashes = False
+    app.config.from_object(config_objec)
     register_extensions(app)
     return app
 
@@ -21,6 +23,15 @@ def register_extensions(app):
     api.add_namespace(director_ns)
     api.add_namespace(genre_ns)
     api.add_namespace(movie_ns)
+    api.add_namespace(user_ns)
+    create_data(app, db)
+
+
+def create_data(app, db):
+    with app.app_context():
+        db.create_all()
+        with db.session.begin():
+            db.session.add_all([])
 
 
 app = create_app(Config())
--- implemented.py
+++ implemented.py
@@ -1,15 +1,49 @@
+import jwt
+from flask import request, abort
+from constants import SECRET, ALGO
 from dao.director import DirectorDAO
 from dao.genre import GenreDAO
 from dao.movie import MovieDAO
+from dao.user import UserDAO
 from service.director import DirectorService
 from service.genre import GenreService
 from service.movie import MovieService
+from service.user import UserService
 from setup_db import db
 
 director_dao = DirectorDAO(session=db.session)
 genre_dao = GenreDAO(session=db.session)
 movie_dao = MovieDAO(session=db.session)
+user_dao = UserDAO(session=db.session)
 
 director_service = DirectorService(dao=director_dao)
 genre_service = GenreService(dao=genre_dao)
 movie_service = MovieService(dao=movie_dao)
+user_service = UserService(dao=user_dao)
+
+
+def auth_required(func):
+    def wrepper(*args, **kwargs):
+        if 'Authorization' not in request.headers:
+            abort(401)
+        return func(*args, **kwargs)
+    return wrepper
+
+
+def admin_required(func):
+    def wrepper(*args, **kwargs):
+        if 'Authorization' not in request.headers:
+            abort(401)
+
+        data = request.headers['Authorization']
+        token = data.split('Bearer ')[-1]
+        try:
+            user = jwt.decode(token, SECRET, algorithms=[ALGO])
+            role_user = user.get("role")
+        except Exception as e:
+            print(f'JWT Decode Exseption: {e}')
+            abort(401)
+        if role_user != 'admin':
+            abort(403)
+        return func(*args, **kwargs)
+    return wrepper
